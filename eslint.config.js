234e77a41af0558cf213dba2f638c54ffca60266
liuziyu <2339554943@qq.com>
2026-05-20 09:48:14 +0800

    update

--- eslint.config.js
+++ eslint.config.js
@@ -1,47 +1,48 @@
 import js from '@eslint/js';
 import stylistic from '@stylistic/eslint-plugin';
 import jsonc from 'eslint-plugin-jsonc';
-import globals from 'globals'; // 引入标准全局变量
+import globals from 'globals';
 import * as jsoncParser from 'jsonc-eslint-parser';
 import tseslint from 'typescript-eslint';
 
 export default [
+  {
+    name: 'app/ignores',
+    ignores: ['dist/**', 'node_modules/**', 'icons/**', 'fonts/']
+  },
+
   js.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    name: 'app/typescript-support-node',
-    files: ['build/**/*.ts', 'utils/**/*.ts', 'eslint.config.js'],
+    name: 'app/node-ts-rules',
+    files: ['build/**/*.ts', 'utils/**/*.ts'],
     languageOptions: {
       parser: tseslint.parser,
       parserOptions: {
         project: './tsconfig.node.json'
       },
-      globals: {...globals.node}
-    }
-  },
-
-  {
-    name: 'app/json-support',
-    files: ['**/*.json'],
-    languageOptions: {
-      parser: jsoncParser
+      globals: {
+        ...globals.node
+      }
     },
     plugins: {
-      jsonc: jsonc
+      '@stylistic': stylistic
     },
     rules: {
-      ...jsonc.configs['recommended-with-json'].rules,
-      'jsonc/indent': [ 'error', 2],
-      'jsonc/quotes': ['error', 'double'],
-      'jsonc/comma-dangle': ['error', 'never']
-    }
-  },
-
-  {
-    name: 'app/custom-rules',
-    files: ['build/**/*.ts', 'utils/**/*.ts', 'eslint.config.js'],
-    plugins: {'@stylistic': stylistic},
-    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      'indent': 'off',
+      'generator-star-spacing': 'off',
+      'no-useless-assignment': 'off',
+      'no-console': 'off',
+      'no-debugger': 'off',
+      'max-len': ['error', {
+        'code': 180,
+        'tabWidth': 2,
+        'ignoreUrls': true,
+        'ignoreStrings': true,
+        'ignoreTemplateLiterals': true,
+        'ignoreComments': true
+      }],
       '@stylistic/indent': ['error', 2, {
         'SwitchCase': 1,
         'VariableDeclarator': 1,
@@ -54,8 +55,6 @@ export default [
       '@stylistic/no-multiple-empty-lines': ['error', {'max': 1, 'maxEOF': 0}],
       '@stylistic/no-trailing-spaces': 'error',
       '@stylistic/no-multi-spaces': 'error',
-      'no-useless-assignment': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
       '@stylistic/quotes': ['error', 'single'],
       '@stylistic/object-curly-spacing': ['error', 'never'],
       '@stylistic/object-curly-newline': ['error', {'multiline': true, 'consistent': true}],
@@ -67,17 +66,34 @@ export default [
       '@stylistic/comma-dangle': ['error', 'never'],
       '@stylistic/space-before-blocks': ['error', 'always'],
       '@stylistic/lines-between-class-members': ['error', 'always', {'exceptAfterSingleLine': true}],
-      '@stylistic/space-before-function-paren': ['error', {'anonymous': 'always', 'named': 'always', 'asyncArrow': 'always'}],
-      'max-len': ['error', {'code': 180, 'tabWidth': 2, 'ignoreUrls': true, 'ignoreStrings': true, 'ignoreTemplateLiterals': true, 'ignoreComments': true}],
-      'no-console': 'off',
-      'no-debugger': 'off',
-      'generator-star-spacing': 'off',
-      'indent': 'off'
+      '@stylistic/space-before-function-paren': ['error', {'anonymous': 'always', 'named': 'always', 'asyncArrow': 'always'}]
     }
   },
 
   {
-    name: 'app/ignores',
-    ignores: ['dist/**', 'node_modules/**', 'public/**', 'assets/cesium/**']
+    name: 'app/config-files',
+    files: ['eslint.config.js', 'svgo.config.js'],
+    languageOptions: {
+      globals: {
+        ...globals.node
+      }
+    }
+  },
+
+  {
+    name: 'app/json-support',
+    files: ['**/*.json', '**/*.jsonc'],
+    languageOptions: {
+      parser: jsoncParser
+    },
+    plugins: {
+      jsonc: jsonc
+    },
+    rules: {
+      ...jsonc.configs['recommended-with-json'].rules,
+      'jsonc/indent': ['error', 2],
+      'jsonc/quotes': ['error', 'double'],
+      'jsonc/comma-dangle': ['error', 'never']
+    }
   }
 ];
