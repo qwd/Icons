4eb52f480e932eac67f7f30317d75e280f9dd353
liuziyu <2339554943@qq.com>
2026-05-20 10:55:14 +0800

    Optimized and updated `build-svg.ts`

--- build/build-svg.ts
+++ build/build-svg.ts
@@ -2,62 +2,63 @@ import {fileURLToPath} from 'node:url';
 import path from 'node:path';
 import fs from 'node:fs';
 import chalk from 'chalk';
-import {load} from 'cheerio';
-import {Config, loadConfig, optimize} from 'svgo';
+import {optimize, type Config, type XastElement, type XastParent} from 'svgo';
+import {writeFile} from '../utils/utils';
 
-// When usePathColor is true, the build svg will retain the fill attribute on the original file path
-const usePathColor = false;
+const [, , arg] = process.argv;
+const keepFill = arg === 'keep-fill';
 const currentFilePath = fileURLToPath(import.meta.url);
 const currentFilename = path.basename(currentFilePath);
 const currentFolderPath = path.dirname(currentFilePath);
 const SvgFolderPath = path.join(currentFolderPath, '../icons/');
 
-const svgAttributes = {
-  xmlns: 'http://www.w3.org/2000/svg',
-  width: '16',
-  height: '16',
-  fill: 'currentColor',
-  class: '',
-  viewBox: '0 0 16 16'
+const SvgConfig:Config = {
+  multipass: true,
+  js2svg: {
+    pretty: true,
+    indent: 2,
+    eol: 'lf'
+  },
+  plugins: [
+    'removeUnknownsAndDefaults',
+    'cleanupListOfValues',
+    'sortAttrs',
+    {
+      name: 'removeAttrs',
+      params: {attrs: ['clip-rule', 'data-name', keepFill ? 'svg:fill' : 'fill']}
+    },
+    {
+      name: 'explicitAttrs',
+      fn () {
+        return {
+          element: {
+            enter (node:XastElement, parentNode:XastParent) {
+              if (node.name === 'svg' && parentNode.type === 'root') {
+                const standardAttrs = {
+                  xmlns: 'http://www.w3.org/2000/svg',
+                  fill: 'currentColor',
+                  width: node.attributes['width'] || '16',
+                  height: node.attributes['height'] || '16',
+                  viewBox: node.attributes['viewBox'] || '0 0 16 16'
+                };
+                node.attributes = standardAttrs;
+              }
+            }
+          }
+        };
+      }
+    }
+  ]
 };
 
-const processFile = (fileName:string, config:Config) => {
+const processFile = (fileName:string) => {
+  if (path.extname(fileName) !== '.svg') {
+    return;
+  }
   const filepath = path.join(SvgFolderPath, fileName);
-  const basename = path.basename(fileName, '.svg');
-
   const originalSvg = fs.readFileSync(filepath, 'utf-8');
-  const $original = load(originalSvg, {xmlMode: true});
-  const originalViewBox = $original('svg').attr('viewBox');
-  svgAttributes.viewBox = originalViewBox ? originalViewBox : '0 0 16 16';
-  const originalPathFillMap = new Map();
-  if (usePathColor) {
-    $original('svg').find('path').each((index, path) => {
-      originalPathFillMap.set(`${index}-${path}`, $original(path).attr('fill'));
-    });
-  }
-
-  const optimizedSvg = optimize(originalSvg, {...config, path: filepath});
-  const $ = load(optimizedSvg.data, {xml: {xmlMode: true}});
-  const $svgElement = $('svg');
-  $svgElement.replaceWith($('<svg>').append());
-
-  for (const [attribute, value] of Object.entries(svgAttributes)) {
-    $svgElement.removeAttr(attribute);
-    $svgElement.attr(attribute, attribute === 'class' ? `qi-${basename}` : value);
-  }
-
-  if (usePathColor && originalPathFillMap.size > 0) {
-    $svgElement.find('path').each((index, path) => {
-      if (originalPathFillMap.has(`${index}-${path}`)) {
-        $(path).attr('fill', originalPathFillMap.get(`${index}-${path}`));
-      }
-    });
-  }
-
-  const resultSvg = $svgElement.toString().replace(/\r\n?/g, '\n');
-  if (resultSvg !== originalSvg) {
-    fs.writeFileSync(filepath, resultSvg, 'utf8');
-  }
+  const optimizedSvg = optimize(originalSvg, {...SvgConfig, path: filepath});
+  writeFile(filepath, optimizedSvg.data);
 };
 
 const run = async () => {
@@ -66,14 +67,9 @@ const run = async () => {
     const timeLabel = chalk.cyan(`[${currentFilename}] finished`);
     console.time(timeLabel);
 
-    const svgConfig = await loadConfig(path.join(currentFolderPath, '../svgo.config.js'));
     const files = fs.readdirSync(SvgFolderPath);
 
-    files.forEach((file) => {
-      if (path.extname(file) === '.svg') {
-        processFile(file, svgConfig);
-      }
-    });
+    files.forEach(processFile);
 
     console.log(chalk.green('\nSuccess, %s icon%s prepared!'), files.length, files.length !== 1 ? 's' : '');
     console.timeEnd(timeLabel);
